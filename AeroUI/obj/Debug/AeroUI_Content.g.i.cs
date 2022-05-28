da1c94786f18071c6b7021e3b0d24e94619d50c5
Pedro Martinez <pedroomtz17@gmail.com>
2022-05-28 12:36:35 -0500

    Agregué fondo a panel FlightData

--- AeroUI/obj/Debug/AeroUI_Content.g.i.cs
+++ AeroUI/obj/Debug/AeroUI_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("imgtakeoff.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/imgflightdata.png")]
 
 
